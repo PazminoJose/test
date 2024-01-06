692272044db2761169f70774a5680e5b6d53176b
José Pazmiño <pepe.paz0399@gmail.com>
2024-01-06 17:11:17 -0500

    feat: create project

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    redirects: async () => {
+        return [
+            {
+                source: '/',
+                destination: '/login',
+                permanent: false,
+            }
+        ]
+    }
+}
+
+module.exports = nextConfig
---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import "@mantine/core/styles.css";
+import "mantine-datatable/styles.layer.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Providers from "./providers";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Capy Drive",
+  description: "Document control app"
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="es">
+      <body className="bg-secondary-100">
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
